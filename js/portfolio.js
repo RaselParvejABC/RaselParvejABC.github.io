94d6d2d718f72e175c5b6abccc152c328e1c0404
RaselParvejABC <raselparvejabc@gmail.com>
2022-06-14 11:46:43 +0600

    Second Commit

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,34 +1,40 @@
 async function fetchMyGithubRepos() {
-  const response = await fetch(
-    "https://api.github.com/users/RaselParvejABC/repos",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        accept: "application/vnd.github.v3+json",
-      },
-    }
-  );
-  const responseJSON = await response.json();
-  console.log(responseJSON);
+
+  let responseJSON = [], response;
+
+  let pageNumber = 1;
+
+  do {
+    response = await fetch(
+      `https://api.github.com/users/RaselParvejABC/repos?page=${pageNumber++}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/vnd.github.v3+json",
+        },
+      }
+    );
+
+    response = await response.json();
+    
+    responseJSON.push(...response);
+
+
+  } while(response.length > 0);
+
+
   return responseJSON;
 }
 
 function getPortfolioItemHTML(project) {
+
   return `
     <div class="col-md-6 col-lg-4 mb-5">
         <div class="portfolio-item mx-auto">
-            <div
-              class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div
-                class="portfolio-item-caption-content text-center text-white">
-                <a href="${project['html_url']}" class="btn btn-success" target="_blank">See on Github</a>
-              </div>
-            </div>
             
-            <div class="portfolio-item-card card">
-                <div class="card-body">
-                    <h4 class="card-title text-primary">${project['name']}</h4>
-                    <p class="card-text">${project['description']}</p>
+            <div class="portfolio-item-card card bg-secondary text-white" data-bs-toggle="tooltip" title="${project['description']}">
+                <div class="card-body flex-fill">
+                    <h4 class="card-title">${project['name']}</h4>
                     <a href="${project['html_url']}" class="card-link" target="_blank">See on Github</a>
                 </div>
             </div>
